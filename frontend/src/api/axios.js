6271df6b66b0afcf718ef62dd44c1c477c7c7839
KLaB <baltasarcosta04@gmail.com>
2026-06-22 04:47:32 +0100

    correcao axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://five0-50-renzo-garage.onrender.com/api",
 });
 
 export default api;
